f0bb5742f043260c746bf415bf2aa6219b26072e
Haithamtut <36354502+Haithamtut@users.noreply.github.com>
2018-02-11 07:33:24 +0200

    Add files via upload

---
+++ New Text Document.js
@@ -0,0 +1,21 @@
+<!-- Start WOWSlider.com BODY section -->
+<div id="wowslider-container1">
+<div class="ws_images"><ul>
+		<li><img src="
+
+https://image.ibb.co/kJdP37/1.png" alt="" title="ÊÌÑíÈí åäÇ ÇáÚäæÇä" id="wows1_0"/></li>
+		<li><a href="http://wowslider.net"><img src="https://preview.ibb.co/ipUWi7/back3.png
+" alt="bootstrap carousel example" title="back3" id="wows1_1"/></a></li>
+		<li><img src="
+https://preview.ibb.co/dC60bS/Kanz.png" alt="Kanz" title="Kanz" id="wows1_2"/></li>
+	</ul></div>
+	<div class="ws_bullets"><div>
+		<a href="#" title=""><span><img src="data1/tooltips/1.png" alt=""/>1</span></a>
+		<a href="#" title="back3"><span><img src="data1/tooltips/back3.png" alt="back3"/>2</span></a>
+		<a href="#" title="Kanz"><span><img src="data1/tooltips/kanz.png" alt="Kanz"/>3</span></a>
+	</div></div><div class="ws_script" style="position:absolute;left:-99%"><a href="http://wowslider.net">slideshow html code</a> by WOWSlider.com v8.8</div>
+<div class="ws_shadow"></div>
+</div>	
+<script type="text/javascript" src="engine1/wowslider.js"></script>
+<script type="text/javascript" src="engine1/script.js"></script>
+<!-- End WOWSlider.com BODY section -->
